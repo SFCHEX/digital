cf7a4804f9cae137896e69eb64c9c3019e1d5667
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-01-04 18:59:40 +0200

    fully working minus the adder

--- loader.sv
+++ loader.sv
@@ -1,21 +1,18 @@
-module loader(input wire permit,input wire [1:16]values,output wire[1:32]out);
-    miniloader m1(permit,values[1],out[1],out[2]);
-    miniloader m2(permit,values[2],out[3],out[4]);
-    miniloader m3(permit,values[3],out[5],out[6]);
-    miniloader m4(permit,values[4],out[7],out[8]);
-	 
-    miniloader m5(permit,values[5],out[9],out[10]);
-    miniloader m6(permit,values[6],out[11],out[12]);
-    miniloader m7(permit,values[7],out[13],out[14]);
-    miniloader m8(permit,values[8],out[15],out[16]);
-
-    miniloader m9(permit,values[9],out[17],out[18]);
-    miniloader m10(permit,values[10],out[19],out[20]);
-    miniloader m11(permit,values[11],out[21],out[22]);
-    miniloader m12(permit,values[12],out[23],out[24]);
-
-    miniloader m13(permit,values[13],out[25],out[26]);
-    miniloader m14(permit,values[14],out[27],out[28]);
-    miniloader m15(permit,values[15],out[29],out[30]);
-    miniloader m16(permit,values[16],out[31],out[32]);
+module loader(input wire permit,input wire [16:1]values,output wire[16:1]set,reset);
+    miniloader m1(permit,values[1],      set[1],   reset[1]);
+    miniloader m2(permit,values[2],      set[2],   reset[2]);
+    miniloader m3(permit,values[3],      set[3],   reset[3]);
+    miniloader m4(permit,values[4],      set[4],   reset[4]);
+    miniloader m5(permit,values[5],      set[5],   reset[5]);
+    miniloader m6(permit,values[6],      set[6],   reset[6]);
+    miniloader m7(permit,values[7],      set[7],   reset[7]);
+    miniloader m8(permit,values[8],      set[8],   reset[8]);
+    miniloader m9(permit,values[9],      set[9],   reset[9]);
+    miniloader m10(permit,values[10],    set[10],  reset[10]);
+    miniloader m11(permit,values[11],    set[11],  reset[11]);
+    miniloader m12(permit,values[12],    set[12],  reset[12]);
+    miniloader m13(permit,values[13],    set[13],  reset[13]);
+    miniloader m14(permit,values[14],    set[14],  reset[14]);
+    miniloader m15(permit,values[15],    set[15],  reset[15]);
+    miniloader m16(permit,values[16],    set[16],  reset[16]);
 endmodule
--- stopwatch.sv
+++ stopwatch.sv
@@ -1,13 +1,14 @@
 module stopwatch(input wire clk, REVERSE,START,RESET,SPEED_UP,SPEED_DOWN,ADD,SUBTRACT,output wire [16:1]Q);
-    wire [31:0]load;
     wire enableCondition1,enableCondition2,enableCondition3;
-	 wire enableCounter1,enableCounter2,enableCounter3;
+	wire enableCounter1,enableCounter2,enableCounter3;
     wire permitter;
-	 wire [16:1]values;
-	 wire [16:1]values_from_adder;
+	wire [16:1]values;
+	wire [16:1]values_from_adder;
     wire ON_OFF;
-
+    wire [16:1]set;
+    wire [16:1]reset;
     reg [15:0] array[1:0];
+    wire index_RESET;
 
     initial begin
         array[0] = 16'b0001_0000_0010_0000; // 1020 BCD binary
@@ -17,36 +18,38 @@ module stopwatch(input wire clk, REVERSE,START,RESET,SPEED_UP,SPEED_DOWN,ADD,SUB
 
   
 //    clock_signals cs(clk_in,SPEED_DOWN,SPEED_UP,clk);
-    bool_equation_reset_values b55(reset,reverse,add,signal,index_reset);
-    bool_equation_selector b44(reset,subtract,add,signal,selector);
-    FullAdderModule fa11(Q[12:9],Q[16:13],add,subtract,values_from_adder[16:1],signal);
-	 
-    m3216 mx(array[index_reset],values_from_adder[16:1],selector,values[16:1]);
+    bool_equation_reset_values b55(RESET,REVERSE,ADD,signal,index_RESET);
+    bool_equation_selector b44(RESET,SUBTRACT,ADD,signal,selector);
+    FullAdderModule fa11(Q[12:9],Q[16:13],ADD,SUBTRACT,values_from_adder[16:1],signal);
+//	 
+    m3216 mx(array[index_RESET],values_from_adder[16:1],selector,values[16:1]);
+//
+    or(permitter,ADD,SUBTRACT,RESET);
+//
+    loader l(permitter,values[16:1],set[16:1],reset[16:1]);
 
-    or(permitter,add,subtract,reset);
-
-    loader l(permitter,values[16:1],load[31:0]);
-	 
-    comparator c12(array[reverse],Q[16:1],force_stop_signal);
+    wire REVERSE_i;
+	not(REVERSE_i,REVERSE);
+    comparator c12(array[REVERSE_i],Q[16:1],force_stop_signal);
 	 
     not(force_stop_signal_i,force_stop_signal);
     and(ON_OFF,START,force_stop_signal_i);
 	 
-    c0_9 c1(clk,ON_OFF,REVERSE,load[7:0]  ,Q[4:1] );
+    c0_9 c1(clk,ON_OFF,REVERSE,set[4:1],reset[4:1],Q[4:1] );
 	 
     boolean_circuit_4 en1(REVERSE,Q[1],Q[2],Q[3],Q[4],enableCondition1);
-	 and(enableCounter1,ON_OFF,enableCondition1);
+	and(enableCounter1,ON_OFF,enableCondition1);
 	 
-    c0_5 c2(clk,enableCounter1,REVERSE,load[15:8] ,Q[8:5]);
+    c0_5 c2(clk,enableCounter1,REVERSE,set[8:5],reset[8:5] ,Q[8:5]);
 	 
     boolean_circuit_7 en2(REVERSE,Q[5],Q[6],Q[7],Q[8],enableCondition2);
-	 and(enableCounter2,ON_OFF,enableCondition1,enableCondition2);
+	and(enableCounter2,ON_OFF,enableCondition1,enableCondition2);
 
-    c0_9 c3(clk,enableCounter2,REVERSE,load[22:15],Q[12:9]);
+    c0_9 c3(clk,enableCounter2,REVERSE,set[12:9],reset[12:9],Q[12:9]);
 	 
     boolean_circuit_11 en3(REVERSE,Q[9],Q[10],Q[11],Q[12],enableCondition3);
-	 and(enableCounter3,ON_OFF,enableCondition1,enableCondition2,enableCondition3);
+	and(enableCounter3,ON_OFF,enableCondition1,enableCondition2,enableCondition3);
 
-    c1_4 c4(clk,enableCounter3,REVERSE,load[31:23],Q[16:13]);
+    c1_4 c4(clk,enableCounter3,REVERSE,set[16:13],reset[16:13],Q[16:13]);
 	 
 endmodule
