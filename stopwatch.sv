a35adf118b5f9cc8f4205ecffc5dfec750103c23
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-01-04 14:13:18 +0200

    IT COMPIIILES NIIIIIGIGIGIIGIAA

--- stopwatch.sv
+++ stopwatch.sv
@@ -15,22 +15,24 @@ module stopwatch(input wire clk_in, REVERSE,START,RESET,SPEED_UP,SPEED_DOWN,ADD,
     end 
 
 
-    
-    clock_signals cs(clk_in,clkslow,clkfast,clk);
+  
+    clock_signals cs(clk_in,SPEED_DOWN,SPEED_UP,clk);
     bool_equation_reset_values b55(reset,reverse,add,signal,index_reset);
     bool_equation_selector b44(reset,subtract,add,signal,selector);
-    fullAdderModule fa11(Q[12:9],Q[16:13],add,subtract,values_from_adder[16:1],signal);
+    FullAdderModule fa11(Q[12:9],Q[16:13],add,subtract,values_from_adder[16:1],signal);
 	 
-    m3216 mx(selector,array[index_reset],values_from_adder[16:1],values[16:1]);
+    m3216 mx(array[index_reset],values_from_adder[16:1],selector,values[16:1]);
 
     or(permitter,add,subtract,reset);
 
     loader l(permitter,values[16:1],load[31:0]);
+	 
     comparator c12(array[reverse],Q[16:1],force_stop_signal);
+	 
     not(force_stop_signal_i,force_stop_signal);
     and(ON_OFF,START,force_stop_signal_i);
-
-    c0_9 c1(clk,start,REVERSE,load[7:0]  ,Q[4:1] );
+	 
+    c0_9 c1(clk,ON_OFF,REVERSE,load[7:0]  ,Q[4:1] );
 	 
     boolean_circuit_4 en1(REVERSE,Q[1],Q[2],Q[3],Q[4],enableCondition1);
 	 and(enableCounter1,ON_OFF,enableCondition1);
