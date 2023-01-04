01b8796f8573ba665e5836f3754536085f503eec
Hamza <202100978@zewailcity.edu.eg>
2023-01-04 21:49:00 +0200

    clock divider and signals

--- clock_signals.sv
+++ clock_signals.sv
@@ -4,7 +4,7 @@ module clock_signals(input clock,slow_down,speed_up,output out_clock);
 reg clock_normal,clock_fast,clock_slow;
 Normal_clock mode_normal(clock,clock_normal);
 Sped_up_clock mode_fast(clock,clock_fast);
-Sped_up_clock mode_slow(clock,clock_slow);
+Slowed_down_clock mode_slow(clock,clock_slow);
 
 m41 mode(clock_normal,clock_fast,clock_slow,clock_normal,speed_up,slow_down,out_clock);
 
