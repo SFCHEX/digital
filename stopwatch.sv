e61ece4b62561907a0a3f22e537d1eee7999bad4
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-01-04 11:39:04 +0200

    commit

--- stopwatch.sv
+++ stopwatch.sv
@@ -1,10 +1,36 @@
 module stopwatch(input wire clk, REVERSE,ON_OFF,RESET,SPEED_UP,SPEED_DOWN,ADD,SUBTRACT,output wire [16:1]Q);
     wire [27:0]load;
     wire enableCondition1,enableCondition2,enableCondition3;
-	 wire enableCounter1,enableCounter2,enableCounter3;
-	 /*      
+	wire enableCounter1,enableCounter2,enableCounter3;
+    wire permitter;
 
-*/    
+    reg [15:0] array[1:0];
+
+    initial begin
+        array[0] = 16'b0001_0000_0010_0000; // 1020 BCD binary
+        array[1] = 16'b0100_0000_0011_0000; // 4030 BCD binary
+    end 
+
+
+    
+	 /*      
+    clkdiv(clk,clkslow,clkfast)
+    bool_equation_reset_values(index_reset1,reset,reverse,add,signal)
+    bool_equation_comparator_values(index_comparator1,reset,reverse,add,signal)
+
+    adderIC(Q[16:1],add,subtract,signal,values_from_adder[16:1]);
+
+    mux(selector,array[index_reset],values_from_adder[16:1],values[16:1]);
+
+
+    or(permitter,add,subtract,reset);
+    loader l(permitter,values[16:1],load[31:0]);
+    comparator(array[index_comparator1],Q[16:1],force_stop_signal)
+
+    not(force_stop_signal_i,force_stop_signal)
+    and(start,ON_OFF,force_stop_signal_i)
+
+*/  
     c0_9 c1(clk,ON_OFF         ,REVERSE,load[7:0]  ,Q[4:1] );
 	 
     boolean_circuit_4 en1(REVERSE,Q[1],Q[2],Q[3],Q[4],enableCondition1);
