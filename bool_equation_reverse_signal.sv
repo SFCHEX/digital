a86ed151b30f21091fe56c176e4f2f27d4f42f65
Hamza <202100978@zewailcity.edu.eg>
2023-01-09 02:31:23 +0200

    bouns

---
+++ bool_equation_reverse_signal.sv
@@ -0,0 +1,12 @@
+module bool_equation_reverse_signals(
+    input pause,reverse,
+    output reverse_signal,error_code_1;
+)
+wire high;
+assign high =1;
+wire error;
+not(enable,pause);   //error if pause is low
+Dlatch DL(reverse,pause,high,reverse_signal);
+and(error_code_1,error);
+
+endmodule
