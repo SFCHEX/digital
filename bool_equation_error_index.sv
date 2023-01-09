b605aa37b957f3fd4eaa8ca82c97aecbd9c5f0fb
CHEX <52354962+SFCHEX@users.noreply.github.com>
2023-01-09 13:12:06 +0200

    save

--- bool_equation_error_index.sv
+++ bool_equation_error_index.sv
@@ -2,6 +2,9 @@ module bool_equation_error_index(
     input error_1,error_2,
     output error_index
 );
-not(error_index,error_1);
-and(error_index,error_2); 
+wire error_1_i;
+not(error_1_i,error_1);
+and(error_index,error_1_i,error_2); 
 endmodule
+
+
